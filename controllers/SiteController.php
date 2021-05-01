b692420da3bf3051156842e2660ffe6f012c6a03
MasterMindNET <romazahrypa@gmail.com>
2021-05-01 12:41:27 +0300

    feat: SiteController added

---
+++ controllers/SiteController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace app\controllers;
+
+
+/**
+ * Class SiteController
+ *
+ * @author Roman Zakhriapa <utmostcreator@gmail.com>
+ * @package app\controllers
+ */
+class SiteController
+{
+    public function handleContact()
+    {
+        return 'handling submitted data with post';
+    }
+
+    public function contact()
+    {
+        return 'contact page';
+    }
+
+}
