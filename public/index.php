e926144fa3001cffab754a8dd528dc34b62c2bbf
UtmostCreator <romazahrypa@gmail.com>
2022-01-22 16:54:23 +0200

    new: favicons added

--- public/index.php
+++ public/index.php
@@ -1,44 +1,40 @@
 <?php
 
 use Framework\Application;
-use Framework\Logger;
-use Framework\Session;
+use Framework\helpers\Config;
 use Whoops\Handler\PrettyPageHandler;
 use Whoops\Run;
 
-/* START of ERROR REPORTING */
-ini_set('display_errors', '1');
-//ini_set('display_startup_errors', '1');
-// disabling error reporting
-// just show "This page isn’t working php-c-framework is currently unable to handle this request. HTTP ERROR 500"
-//ini_set('display_errors', '0');
-error_reporting(E_ALL);
-/* END of ERROR REPORTING */
-
 require_once __DIR__ . './../vendor/autoload.php';
-$config = require_once './../config/config.php';
-Session::initIfItDoesNotExist(); // TODO move to more specific place
-
-// System logger
-$logger = Logger::getInst();
-// System Error Handler
-// (new \Framework\ProductionErrorHandler($logger))->register();
-//(new ErrorHandler($logger))->register();
-// System application
-
+Config::init();
 // TODO check if $whoops->pushHandler(new PrettyPageHandler()); can be placed in Router::resolveController method
 if (isDev()) {
+    /* START of ERROR REPORTING */
+    // just show "This page isn’t working php-c-framework is currently unable to handle this request. HTTP ERROR 500"
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+    /* END of ERROR REPORTING */
     $whoops = new Run();
     $whoops->pushHandler(new PrettyPageHandler());
     $whoops->register();
-//                throw new \Exception($t->getMessage() . "<br/>" . $t->getFile());
+} elseif (isProd()) {
+    ini_set('display_errors', '0');
+    ini_set('display_startup_errors', '0');
+    error_reporting(0);
 }
 
-Application::create(dirname(__DIR__), $config);
+//\Modules\DD::dd(Config::get());
+Application::create();
 $app = Application::getInstance();
+//$app->bind('paths.base', fn() => __DIR__ . '/..');
+// TODO fix with the ENV var
+$app->bind('paths.base', fn() => \Framework\Paths::getBase());
+//\Modules\DD::dd(app('paths.base'));
 //(new \Framework\db\Query())->insert('users', ['email', 'firstname', 'lastname', 'status', 'password',],
 //    ['email@gmail.com', 'fadsfasd', 'adsfasdfasdf', '1', 'sdfasdfasdd']);
 
-require_once base_path() . '/app/routes/web.php';
+// TODO mull over on a better way to add it
+require_once basePath() . '/app/routes/web.php';
 
 $app->run();
